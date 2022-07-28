b1bb211edd7b659247689d0d960cdb9f88036cf5
robyn anderson <robynjanders@gmail.com>
2022-07-28 14:12:28 -0400

    Connected to mongoDB Atlas.

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const express = require("express"),
+    app = express(),
+    mongoose = require("mongoose"),
+    cors = require("cors"),
+    PORT = process.env.PORT
+    CONNECT = process.env.CONNECT,
+    
+mongoose.connect(`${CONNECT}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection-error"));
+
+app.listen(PORT, () => {
+  try {
+    console.log(`Server running on ${PORT}`);
+  } catch (err) {
+    console.log(`Server error: ${err}`);
+  }
+});

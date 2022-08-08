240b13e4bc78c0a738a2e3ceb913e8976654dc63
robyn-anderson <106846264+robyn-anderson@users.noreply.github.com>
2022-08-08 15:01:52 -0400

    Modify ticket endpoint has been added. A lso added universal error handler from auth.js to bottom of file. Added comments.

--- server/controllers/ticket.js
+++ server/controllers/ticket.js
@@ -9,7 +9,7 @@ const session = require('../middlewares/session');
 
 //  Create a new ticket
 router
-  .route("/create-ticket")
+  .route("/create")
   .post([session], async (req, res, next) => {
     const { 
       requestor, 
@@ -48,6 +48,7 @@ router
         }
     }
     } catch (err) {
+      // Pass error to error-handling middleware at the bottom
       next(err);
     }
   }
@@ -55,7 +56,7 @@ router
   
   // Get all tickets
   router
-  .route("/all-tickets")
+  .route("/all")
   .get([session], async (req, res, next) => { 
     try {
       let allTickets = await Ticket.find({});
@@ -63,70 +64,85 @@ router
         allTickets,
       });
     } catch(err) {
+      // Pass error to error-handling middleware at the bottom
       next(err);
     }
   })
 
   // Get all tickets by the status filters set (passed through as queries in url)
-    router
-    .route("/status-filter")
-    .get([session], async (req, res, next) => { 
-      try {
-        let statuses = req.query.status
-        let newRequestTickets = await Ticket.find({status: { $in: statuses}});
-        res.status(200).json({
-          newRequestTickets,
-        });
-      } catch(err) {
-        next(err);
-      }
-    })
-
-  // Get one ticket using ticketid as query in url
   router
-  .route("/")
-  .get([session], async (req, res) => { 
+  .route("/status-filter")
+  .get([session], async (req, res, next) => { 
     try {
-      const ticketid = req.query.ticketid;
-      let ticket = await Ticket.find({_id: ticketid});
+      let statuses = req.query.status
+      let newRequestTickets = await Ticket.find({status: { $in: statuses}});
+      res.status(200).json({
+        newRequestTickets,
+      });
+    } catch(err) {
+      // Pass error to error-handling middleware at the bottom
+      next(err);
+    }
+  })
+
+  // Get one ticket using ticketid as param
+  router
+  .route("/:id")
+  .get([session], async (req, res, next) => { 
+    try {
+      const { id } = req.params;
+      let ticket = await Ticket.find({_id: id});
       res.status(200).json({
         ticket,
       });
     } catch(err) {
+      // Pass error to error-handling middleware at the bottom
       next(err);
     }
   })
 
 
-/* router
-  .route("/modify-ticket")
+  router
+  .route("/modify/:id")
   .put([session], async (req, res, next) => {
-    const { ticketID } = req.body;
-    
+    // Find ticket by document ID passed as parameter
+    const { id } = req.params;
     try {
-    
-      let ticketModify = await Ticket.findOne({ ticketID: ticketID });
-    
-      if (!ticketModify){  
-        throw new Error("no ticket with ID exists");
-      } else {
-        for (field in req.body){
-        ticketModify[field] = req.body[field];
+      let ticketToModify = await Ticket.findOne({ _id: id });
+      
+      // Error if no ticket with that ID found
+      if (!ticketToModify) {
+        throw new Error("no ticket with that id exists");
+      } 
+      else {
+        // loop through request body and only update fields that exist in the request
+        for (field in req.body) {
+            ticketToModify[field] = req.body[field];
+          }
         }
-        try {
-          await ticketModify.save();
-          res.status(202).json({
-          status: "ticket modified",
-          ticketModify,
-          });
-      } catch (error) {
-        const missingData = Object.keys(error.errors)
-        throw new Error(`ticket could not be updated because: you are missing the following data: ${[...missingData]}`)
-      }
+        // Save updates to ticket document
+        await ticketToModify.save();
+        res.status(202).json({
+          status: "ticket updated",
+          ticketToModify,
+        });
+      
+    } catch (err) {
+      // Pass error to error-handling middleware at the bottom
+      next(err);
     }
-  } catch (err) {
-    next(err);
-  }
-}) */
+  });
+
+  //TODO: Delete ticket
+  //TODO: Permission control for editing tickets/fields within tickets & deleting tickets
+
+// Universal error handler
+// Any error thrown above goes through this
+router.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    status: err.message,
+  });
+});
 
 module.exports = router

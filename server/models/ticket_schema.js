06fa52a7cc26433c374806de3ab6855d49cdbe3b
robyn-anderson <106846264+robyn-anderson@users.noreply.github.com>
2022-08-17 17:42:54 -0400

    Added a user-friendly ticket ID with incrementing. Need to think about error handling for ID uniqueness

--- server/models/ticket_schema.js
+++ server/models/ticket_schema.js
@@ -4,6 +4,7 @@ const CustomFieldsSchema =require('./custom_fields_schema')
 const FieldsToIgnore = [
   '_id',
   '__v',
+  'ID',
   'Department',
   'Requestor',
   'createdAt',
@@ -12,6 +13,10 @@ const FieldsToIgnore = [
 
 let reqTicket = new mongoose.Schema(
   {
+    ID: {
+      type: Number,
+      required: true
+    },
     Requestor: {
       type: mongoose.ObjectId,
       required: true

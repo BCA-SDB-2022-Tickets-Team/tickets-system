5ea9da1a1ce1fff14047cffd1c1af23c981c66e6
robyn-anderson <106846264+robyn-anderson@users.noreply.github.com>
2022-08-21 13:10:40 -0400

    Set back to previous commit (3c4f2a5)

---
+++ client/src/components/AllTickets/TicketsTable.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState, useContext } from 'react'
+import { NavLink } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Table } from "reactstrap"
+import "./AllTickets.css"
+
+
+const TicketsTable = (props) => {
+
+    const [ticketData, setTicketData] = useState([])
+    const [ticketFieldHeadings, setTicketFieldHeadings] = useState([])
+
+    useEffect(() => {
+
+        async function getData() {
+
+            let res = await fetch("http://localhost:4000/api/ticket/all", {
+                method: "GET",
+                headers: new Headers({
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${localStorage.getItem('token')}`
+                })
+            });
+
+            res.json()
+                .then(data => {
+                    return data.allTicketsData.map(ticket => {
+                        console.log(ticket.Status)
+                        console.log(props.slectedFilters)
+                        //if (props.filters.includes(ticket.Status) === true) {
+                        return {
+                            _id: ticket._id,
+                            'Ticket ID': ticket.ID,
+                            'Assessor': !ticket.Assessor
+                                ? <em>Unassigned</em>
+                                : data.allUsers.map(user => {
+                                    if (ticket.Assessor === user._id) {
+                                        return `${user.firstName} ${user.lastName}`
+                                    }
+                                }),
+                            'Created At': ticket['Created At'],
+                            'Vendor Name': ticket['Vendor Name'],
+                            'Updated At': ticket['Updated At'],
+                            'Status': ticket.Status
+                        }
+                        //}
+                    })
+                })
+                .then(mappedData => {
+                    setTicketData(mappedData)
+                    return mappedData
+                })
+                .then(mappedData => {
+                    setTicketFieldHeadings(Object.keys(mappedData[0]))
+                })
+        }
+        getData();
+    }, []);
+
+    return (
+        <Table striped responsive hover>
+            <tbody>
+                <tr>
+                    {/* Map over the ticket field headings and display as table headings */}
+                    {ticketFieldHeadings.map(heading => {
+                        if (heading !== "_id") {
+                            return (
+                                <th className="all-tickets-headings" key={heading}>{heading}</th>
+                            )
+                        }
+                    })}
+                </tr>
+                {/* Map over the tickets and display their data in a table */}
+                {ticketData.map((ticket) => {
+                    let contentKeys = Object.keys(ticket)
+
+                    return (
+                        <tr key={ticket['Created At']} >
+                            {contentKeys.map(field => {
+                                if (field !== "_id") {
+                                    return (
+                                        <td className="all-tickets-data" key={field} >
+                                            {/* <NavLink onClick={props.setTicketID(ticket._id)} to={`/oneticket?id=${ticket['_id']}`}> */}
+                                            {field === 'Created At' || field === 'Updated At'
+                                                ? new Date(ticket[field]).toLocaleDateString()
+                                                : ticket[field]}
+                                            {/* </NavLink> */}
+                                        </td>
+                                    )
+                                }
+                            })}
+                        </tr>
+                    )
+
+                })}
+            </tbody>
+        </Table>
+    )
+}
+
+export default TicketsTable
